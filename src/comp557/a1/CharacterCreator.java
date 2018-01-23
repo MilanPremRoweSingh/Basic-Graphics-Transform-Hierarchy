aaf368fe224400c81bc40daf16e513a8114e3f3a
Milan Singh <milanprsingh@gmail.com>
2018-01-23 16:40:32 -0500

    Initial Commit

---
+++ src/comp557/a1/CharacterCreator.java
@@ -0,0 +1,17 @@
+package comp557.a1;
+
+public class CharacterCreator {
+
+	static public String name = "CHARACTER NAME - YOUR NAME AND STUDENT NUMBER";
+	
+	/** 
+	 * Creates a character.
+	 * @return root DAGNode
+	 */
+	static public DAGNode create() {
+		// TODO: use for testing, and ultimately for creating a character
+		// Here we just return null, which will not be very interesting, so write
+		// some code to create a charcter and return the root node.
+		return null;
+	}
+}
